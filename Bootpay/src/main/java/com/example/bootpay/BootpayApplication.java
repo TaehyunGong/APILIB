a4f2a78684bda356ce8afb92c53ccbc69b81f927
tony <tony950620@naver.com>
2021-12-09 17:45:58 +0900

    bootpay module init commit

---
+++ Bootpay/src/main/java/com/example/bootpay/BootpayApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootpayApplication.class, args);
+    }
+
+}
---
+++ Bootpay/src/test/java/com/example/bootpay/BootpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
